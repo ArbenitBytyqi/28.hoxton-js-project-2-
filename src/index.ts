ffe2196ebc2088310f7f05aedee401f09b063fb2
Aris Alshiqi <arisalshiqi111@gmail.com>
2022-07-20 12:45:09 +0200

    Database generated from typescript functions

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,48 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+let tbody = document.getElementById("tbody")
+
+
+
+// fetch function
+fetch("http://localhost:3000/user")
+    .then(res => res.json())
+    .then(json => {
+        json.map(data => {
+            console.log(data)
+            tbody.append(td_fun(data));
+        })
+    })
+
+// create td
+function td_fun({ profile, name, email, status, role}){
+    let td = document.createElement('tr');
+    td.innerHTML = `
+    <td class="px-6 py-4 whitespace-nowrap">
+        <div class="flex items-center">
+                <div class="flex-shrink-0 h-10 w-10">
+                    <img src="${profile}" class="h-10 w-10 rounded-full" alt="">
+                </div>
+                <div class="ml-4">
+                    <div class="text-sm font-medium text-gray-900">
+                        ${name}
+                    </div>
+                    <div class="text-sm text-gray-500">
+                        ${email}
+                    </div>
+                </div>
+            </div>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
+        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+           ${status}
+        </span>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
+        <span class="text-sm text-gray-500">${role}</span>
+    </td>
+    `;
+    return td;
+}
